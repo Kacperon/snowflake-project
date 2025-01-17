f965e9bed51efb91f325aab4056c920ec286b804
nicodiangelo333 <anielasz45@gmail.com>
2025-01-17 19:24:56 +0100

    animacja wstępna

--- python/animate.py
+++ python/animate.py
@@ -1,5 +1,6 @@
 from generate_lib.import_lib import *
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
 def display(segments):
     segments = sum(segments, [])
@@ -11,4 +12,36 @@ def display(segments):
     plt.show()
 
 def animate(segments):
+  
+    # fig, ax = plt.subplots(figsize=(6, 6))
+    
+    fig = plt.figure(facecolor='#111111')
+    ax = plt.gca()
+    ax.axis('off')
+    ax.axis('square')
+    ax.set_xlim(-30, 30)
+    ax.set_ylim(-30, 30)
+    lines = []
+
+    # plot initialization
+    def init():
+        for _ in range(len(segments)):
+            line, = ax.plot([], [], lw=2)
+            lines.append(line)
+        return lines
+
+    # updates
+    def update(frame):
+        for idx, line in enumerate(lines[:frame + 1]):
+            if idx < len(segments):
+                xdata, ydata = [], []
+                for seg in segments[idx]:
+                    xdata.extend([seg.x1, seg.x2, None])  # None for discontinuous segments
+                    ydata.extend([seg.y1, seg.y2, None])
+                line.set_data(xdata, ydata)
+        return lines
+    # to animuje względnie
+    ani = FuncAnimation(fig, update, frames=len(segments), init_func=init, blit=True, interval=500)
+
+    plt.show()
     print(":D")
--- python/main.py
+++ python/main.py
@@ -2,3 +2,4 @@ from generate import *
 from animate import *
 
 display(generate())
+animate(generate())
