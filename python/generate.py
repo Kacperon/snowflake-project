d8872ed20f2ace491d63faa609761ec8a241f2aa
Jaaaaas <x.janek.z@gmail.com>
2025-01-24 17:06:26 +0100

    v13

--- python/generate.py
+++ python/generate.py
@@ -2,6 +2,9 @@ from generate_lib.import_lib import *
 
 # generates segments of a snowflake
 def generate(layers):
+    segments = []
+    # repeat all if failed
+    while len(segments) < layers:
     # randomize weather
         conditions = weather(layers)
     # starting element
@@ -9,12 +12,13 @@ def generate(layers):
     # generate elements
         for i in range(1,layers):
             segments = generate_element(segments, i, conditions)
-    # remove temporary
+    # remove temporary and empty
         for i in range(len(segments)):
             segments[i] = [segment for segment in segments[i] if segment.temporary==0]
+        segments = [element for element in segments if element]
     # rotate randomly
         for i in range(len(segments)):
             seed(sum(sum(conditions[:],[])))
             angle = randint(0,360)
             segments[i] = [rotate(segment,angle) for segment in segments[i]]
-        return segments
+    return segments
